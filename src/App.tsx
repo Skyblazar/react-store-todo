d82460f44e284479cf0182b74d1f0de8d78d0678
King Elisha <king.elisha@seeq.com>
2024-04-19 23:12:35 +0100

    Basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h3>Main App</h3>} />
+        <Route path="/login" element={<h3>Login</h3>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
